b4c4e8ec9a7c6790bd211a923d7a5ec86f6c45a5
Hoang Le <hqle@akvagroup.com>
2024-10-15 20:47:54 +0700

    Refactoring

--- lib/src/encodable.dart
+++ lib/src/encodable.dart
@@ -2,6 +2,17 @@
 
 part of "../binary_map_file.dart";
 
+/// Interface which support encode for binary map file
+///
+/// Supported messages are acyclic values of these forms:
+///
+///  * null
+///  * [bool]s
+///  * [num]s
+///  * [String]s
+///  * [Uint8List]s, [Int32List]s, [Int64List]s, [Float64List]s
+///  * [List]s of supported values
+///  * [Map]s from supported values to supported values
 abstract interface class IEncodable {
-  Map<dynamic, dynamic> toMap();
+  Map toMap();
 }
